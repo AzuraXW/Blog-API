713faaa3fcfb5284f3e499d18d7539a3f211a07d
DG-GK <76480605+DG-GK@users.noreply.github.com>
2022-01-05 19:37:12 +0800

    some message

--- routers/admin/article.js
+++ routers/admin/article.js
@@ -4,20 +4,55 @@ const router = new Router({
 })
 const Article = require('../../models/article')
 
-// const test = new Article({
-//   title: '测试',
-//   tag_id: '2',
-//   author_id: '2',
-//   content: '测试内测试内容测试内容测试内容容'
-// })
-// test.save()
-
 // 获取文章列表
 router.get('/articles', async (ctx) => {
   const articles = await Article.find({})
   ctx.body = {
     data: articles
   }
+})
+
+// 添加文章
+router.post('/create', async (ctx) => {
+  const { title, content, tag_id, author_id } = ctx.request.body
+  const result = await Article.create({ title, content, tag_id, author_id })
+  if (result) {
+    ctx.body = {
+      data: {
+        code: 200,
+        msg: '添加成功'
+      }
+    }
+  } else {
+    ctx.body = {
+      code: 300,
+      msg: '添加失败'
+    }
+  }
+})
+
+// 查找单个文章
+router.get('/article', async (ctx) => {
+  const id = ctx.query.id
+  await Article.findOne({ _id: id }).then(rel => {
+    if (rel) {
+      ctx.body = {
+        code: 200,
+        data: rel
+      }
+    } else {
+      ctx.body = {
+        code: 300,
+        msg: '查找失败'
+      }
+    }
+  }).catch(err => {
+    ctx.body = {
+      code: 500,
+      msg: '查找时出现异常',
+      err
+    }
+  })
 })
 
 module.exports = router
