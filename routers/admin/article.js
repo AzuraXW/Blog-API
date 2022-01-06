43089e977b67540a93add18e9c690569173b3bfc
Wangwei <2219444825@qq.com>
2022-01-06 13:09:47 +0800

    修改代码

--- routers/admin/article.js
+++ routers/admin/article.js
@@ -1,58 +1,70 @@
 const Router = require('koa-router')
 const router = new Router({
-  prefix: '/api/v1/admin'
+  prefix: '/api/v1/admin/article'
 })
 const Article = require('../../models/article')
 
 // 获取文章列表
-router.get('/articles', async (ctx) => {
-  const articles = await Article.find({})
+router.get('/list', async (ctx) => {
+  const { page = 1, limit = 10 } = ctx.query
+  const articles = await Article
+    .find()
+    .skip(page - 1)
+    .limit(parseInt(limit))
   ctx.body = {
+    code: 200,
+    message: '本次获取' + articles.length + '条文章数据',
+    count: articles.length,
     data: articles
   }
 })
 
 // 添加文章
 router.post('/create', async (ctx) => {
-  const { title, content, tag_id, author_id } = ctx.request.body
-  const result = await Article.create({ title, content, tag_id, author_id })
+  const {
+    title,
+    content,
+    tag_id: tagId,
+    author_id: authorId
+  } = ctx.request.body
+  const result = await Article.create({
+    title,
+    content,
+    tag_id: tagId,
+    author_id: authorId
+  })
   if (result) {
     ctx.body = {
+      code: 200,
+      message: '新建文章成功',
       data: {
-        code: 200,
-        msg: '添加成功'
+        create_id: result._id
       }
     }
   } else {
     ctx.body = {
-      code: 300,
-      msg: '添加失败'
+      code: 200,
+      message: '新建文章错误'
     }
   }
 })
 
 // 查找单个文章
-router.get('/article', async (ctx) => {
+router.get('/detail', async (ctx) => {
   const id = ctx.query.id
-  await Article.findOne({ _id: id }).then(rel => {
-    if (rel) {
-      ctx.body = {
-        code: 200,
-        data: rel
-      }
-    } else {
-      ctx.body = {
-        code: 300,
-        msg: '查找失败'
-      }
-    }
-  }).catch(err => {
+  const result = await Article.findById(id)
+  if (result) {
     ctx.body = {
-      code: 500,
-      msg: '查找时出现异常',
-      err
+      code: 200,
+      message: '获取成功',
+      data: result
     }
-  })
+    return
+  }
+  ctx.body = {
+    code: 300,
+    message: '查找失败'
+  }
 })
 
 module.exports = router
